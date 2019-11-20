487f7c093a75799a8cf83f9b664e406f36dcd8a1
chris barbour <cbarbourcr@gmail.com>
2019-11-20 10:19:21 -0500

    Updated python scripts for deployment

--- Update_Predictions.py
+++ Update_Predictions.py
@@ -9,8 +9,8 @@ import os
 
 ## Extract Titanic Data
 def Extract_Data():
-    # data_uri = os.environ['DATABASE_URL']
-    data_uri = 'postgres://ujfkzspzjssvhx:ece2bb84158cf21405a686384ea4eff5344303580151c5180b40cbf9c78c7519@ec2-107-20-167-241.compute-1.amazonaws.com:5432/d9lhgv0dafi2fv'
+    data_uri = os.environ['DATABASE_URL']
+    # data_uri = 'postgres://ujfkzspzjssvhx:ece2bb84158cf21405a686384ea4eff5344303580151c5180b40cbf9c78c7519@ec2-107-20-167-241.compute-1.amazonaws.com:5432/d9lhgv0dafi2fv'
     engine = create_engine(data_uri)
     dat = pd.read_sql('''SELECT sfid, heroku_id__c, survival_probability__c, survived__c,age__c,sex__c, pclass__c, fare__c
                          FROM salesforce.titanic_train_cleaned__c''', engine)
@@ -22,8 +22,8 @@ dat = Extract_Data()
 ## Function for updating Salesforce records
 def Update_Salesforce_Records(Prediction_Output):
     try:
-        # data_uri = os.environ['DATABASE_URL']
-        data_uri = 'postgres://ujfkzspzjssvhx:ece2bb84158cf21405a686384ea4eff5344303580151c5180b40cbf9c78c7519@ec2-107-20-167-241.compute-1.amazonaws.com:5432/d9lhgv0dafi2fv'
+        data_uri = os.environ['DATABASE_URL']
+        # data_uri = 'postgres://ujfkzspzjssvhx:ece2bb84158cf21405a686384ea4eff5344303580151c5180b40cbf9c78c7519@ec2-107-20-167-241.compute-1.amazonaws.com:5432/d9lhgv0dafi2fv'
         postgres_connection = psycopg2.connect(data_uri)
         cursor = postgres_connection.cursor()
         print('Connected to Database')
